3c28fe8627e81d9177da44f11a16d42e74487629
Harrison Nelson <harrisonsam2002@gmail.com>
2020-05-06 22:01:23 -0400

    day 2 final

--- Assignments/5studentmarks.py
+++ Assignments/5studentmarks.py
@@ -9,13 +9,13 @@ end = 5
 
 
 while start < end:
-  #num.append(input("What is your student number? "))
-  #name.append(input("What is your name? "))
-  m1.append(int(input("What is mark 1? ")))
-  m2.append(int(input("What is mark 2? ")))
-  m3.append(int(input("What is mark 3? ")))
-  print ("")
-  start+=1
+    #num.append(input("What is your student number? "))
+    #name.append(input("What is your name? "))
+    m1.append(int(input("What is mark 1? ")))
+    m2.append(int(input("What is mark 2? ")))
+    m3.append(int(input("What is mark 3? ")))
+    print("")
+    start += 1
 
 avg = []
 avg.append(((m1[0]+m2[0]+m3[0])/3))
@@ -30,13 +30,13 @@ for each in avg:
         print("Class A")
     if each > 50 and each <= 60:
         print("Class B")
-    if each > 35 and each <=50:
+    if each > 35 and each <= 50:
         print("Class C")
     if each <= 35:
         print("Fail")
-    index+=1
-        
-total =[]
+    index += 1
+
+total = []
 total.append(((m1[0]+m2[0]+m3[0])))
 total.append(((m1[1]+m2[1]+m3[1])))
 total.append(((m1[2]+m2[2]+m3[2])))
@@ -44,4 +44,4 @@ total.append(((m1[3]+m2[3]+m3[3])))
 total.append(((m1[4]+m2[4]+m3[4])))
 
 for each in total:
-    print("Your total mark is:",each)
+    print("Your total mark is:", each)
---
+++ Samples/test.py
@@ -0,0 +1,6 @@
+
+import json
+import math
+print(math.pi)
+print("hit")
+json.
