a8b195703afb14d8c6a6bf8d194be16b2c989ee9
Harrison Nelson <harrisonsam2002@gmail.com>
2020-11-14 13:41:14 -0500

    day 1.1

---
+++ Samples/Day 1 - Variables and Print.py
@@ -0,0 +1,9 @@
+
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("Print('what to print')")
+
+print("Day 1 - Python Print Function\n""The function is declared like this:\n""Print('what to print')")
+
+print("Hello" + " Angela")
