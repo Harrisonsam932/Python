7582167b08aa4f2c74d82d5191bedbe7eaa76848
Harrison Nelson <harrisonsam2002@gmail.com>
2021-02-20 21:08:07 -0500

    Commit

--- Samples/Day 1 - Variables and Print.py
+++ Samples/Day 1 - Variables and Print.py
@@ -1,9 +1,14 @@
 
+print("hello", input("what is your name: "))
+print("hello, world!\nHello, boy")
 
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("Print('what to print')")
 
-print("Day 1 - Python Print Function\n""The function is declared like this:\n""Print('what to print')")
 
-print("Hello" + " Angela")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("Print('what to print')")
+
+# print("Day 1 - Python Print Function\n""The function is declared like this:\n""Print('what to print')")
+
+# print("Hello", "Harrison")
+
